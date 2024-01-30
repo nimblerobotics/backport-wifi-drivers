8c6945380c9cbe75909219dddbebe71f02e8047f
Gen Xu <gen@nimble.ai>
2024-01-29 20:17:48 -0800

    add installed size to make apt happy

--- debian.sh
+++ debian.sh
@@ -6,6 +6,7 @@ cp -r firmware/* "$tmp_folder"/lib/firmware/updates
 mkdir -p "$tmp_folder"/lib/modules/$kernel_version/updates
 # Find all .ko files and copy them to the correct folder that keeps hierarchy
 find . -name "*.ko" -exec cp -v --parents {} "$tmp_folder"/lib/modules/$kernel_version/updates \;
+size=$(du -ks "$tmp_folder" | cut -f1)
 # Copy the debian folder to the tmp folder
 mkdir -p "$tmp_folder"/DEBIAN
 cp debian/* "$tmp_folder"/DEBIAN
@@ -13,6 +14,7 @@ pushd "$tmp_folder" || exit
 # Create the debian package
 name=$(grep -oP '(?<=Package: ).*' "$tmp_folder"/DEBIAN/control)
 version=$(grep -oP '(?<=Version: ).*' "$tmp_folder"/DEBIAN/control)
+sed -i "s/Installed-Size: .*/Installed-Size: $size/g" "$tmp_folder"/DEBIAN/control
 dpkg-deb --build "$tmp_folder" "$name"-"$version"+$kernel_version.deb
 popd || exit
 mv "$tmp_folder"/"$name"-"$version"+$kernel_version.deb ./
